e3ea2afd21274a6d55051b0996da361e5b0cb0f4
vishal5208 <109859421+vishal5208@users.noreply.github.com>
2022-07-29 19:31:11 +0530

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,9 +4,10 @@ from scripts.helpful_scripts import get_account
 
 def deploy_token():
     account = get_account()
-    vishal = VishalToken.deploy(
+    token = VishalToken.deploy(
         config["token_name"], config["token_symbol"], 18, account.address, {"from": account},  publish_source=config["networks"][network.show_active()].get("verify"),)
-    print(vishal)
+    token.mintInitialSupply(account.address)
+
 
 
 def main():
